e96091c06e6984fa649d5f24c2a1413741e9d866
agil <antonio_g_s@hotmail.com>
2020-03-12 23:43:43 +0100

    Final

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Antonio Gil Sánchez on 04/03/2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
